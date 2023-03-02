756ca13ca273cf164d8919c4fc16712348e9b51d
EsinShadrach <esinshadrachrafe@gmail.com>
2023-03-02 23:53:21 +0100

    Added loading effect ish

--- src/body.jsx
+++ src/body.jsx
@@ -1,6 +1,7 @@
 export default function Body() {
     return (
-        <main className="p-3e px-4e w-full md:ml-52">
+        <main className="w-full bg-black md:pl-[13.05rem]">
+            <div className="absolute bg-gray-500 bg-opacity-50 backdrop-blur-sm w-full h-full animate-pulse"></div>
             <iframe className="h-screen w-full mt-[4.5rem] md:m-0 relative" allowFullScreen={true} allow="accelerometer; magnetometer; gyroscope" src="https://panoraven.com/en/embed/PYKxs5WSfX"></iframe>
         </main>
     )
