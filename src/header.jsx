5e24fe466798a8a9d8c0baa6403328666c67fb57
EsinShadrach <esinshadrachrafe@gmail.com>
2023-03-03 02:16:01 +0100

    Created User Prompt

--- src/header.jsx
+++ src/header.jsx
@@ -1,114 +1,165 @@
-import NavBrand from './assets/images/transparent.png'
-import discord from './assets/images/discord.png'
-import { Disclosure, Transition } from '@headlessui/react'
-import { ChevronDown } from 'heroicons-react'
+import NavBrand from "./assets/images/transparent.png";
+import discord from "./assets/images/discord.png";
+import { Disclosure, Transition } from "@headlessui/react";
+import { ChevronDown } from "heroicons-react";
+import MyModal from "./prompt";
+import { useState } from "react";
 
 export default function NavBar() {
-    return (
-        // Making 2 navbars 
-        <header className='fixed w-full z-50'>
-            <nav className="hidden md:block bg-black text-white fixed h-screen p-3">
-                <div className='flex flex-col h-full gap-36 overflow-auto'>
-                    <div className='flex items-center justify-center gap-2'>
-                        <div>
-                            <img className='w-16 h-16' src={NavBrand} alt="" />
-                        </div>
-                        <div className='font-bold text-xl mr-2'>
-                            Dall-E - 360
-                        </div>
-                    </div>
-                    <div className='h-fulls flex items-center mb-20'>
-                        <div className='flex flex-col gap-3'>
-                            <a href='#' className='flex  items-center gap-2 hover:translate-x-2 duration-300'>
-                                <div className='h-1.5 w-1.5 bg-rose-600 rounded-full'></div>
-                                {/* <div className='h-1 w-1 bg-rose-600 rounded-full'></div> Using this for active links*/}
-                                Home
-                            </a>
-                            <a href='#' className='flex  items-center gap-2 hover:translate-x-2 duration-300'>
-                                <div className='h-1.5 w-1.5 bg-white rounded-full'></div>
-                                Early Access
-                            </a>
-                            <a href='#' className='flex  items-center gap-2 hover:translate-x-2 duration-300'>
-                                <div className='h-1.5 w-1.5 bg-white rounded-full'></div>
-                                Gallery
-                            </a>
-                        </div>
-                    </div>
-                    <a href='#' className='flex items-center gap-1 px-3 p-3 text-gray-200 fixed bottom-0'>
-                        <img className='w-10' src={discord} alt="" />
-                        <div className='bg-gray-300 w-1 h-1 rounded-full'></div>
-                        <div>
-                            Discord
-                        </div>
-                    </a>
-                </div>
-            </nav>
-            <nav className='text-white bg-black w-full p-3 md:hidden'>
-                <div>
-                    <Disclosure>
-                        {({ open }) => (
-                            <>
-                                <div className='flex justify-between'>
-                                    <div className='flex items-center gap-2 font-bold text-lg'>
-                                        <div>
-                                            <img className='w-12 h-12' src={NavBrand} alt="" />
-                                        </div>
-                                        <div>
-                                            Dall-E - 360
-                                        </div>
-                                    </div>
-                                    <Disclosure.Button>
-                                        <div className={
-                                            `${open?'rotate-180':'rotate-0'}
-                                            transition-all duration-500 rounded-full border-2 p-0.5 flex items-center justify-center border-t-red-400`
-                                            }>
-                                            <ChevronDown />
-                                        </div>
-                                    </Disclosure.Button>
-                                </div>
-                                <Transition
-                                    show={open}
-                                    enter="transition duration-300 ease-out"
-                                    enterFrom="transform translate-y-full opacity-0"
-                                    enterTo="transform translate-y-0 opacity-100"
-                                    leave="transition duration-300 ease-out"
-                                    leaveFrom="transform translate-y-full opacity-100"
-                                    leaveTo="transform scale-50 opacity-0"
-							    >
-                                    <Disclosure.Panel className='mt-2 p-2 font-medium space-y-2'>
-                                        <a href='#' className='flex  items-center gap-2 hover:translate-x-2 duration-300'>
-                                            <div className='h-1.5 w-1.5 bg-rose-600 rounded-full'></div>
-                                            {/* <div className='h-1 w-1 bg-white rounded-full'></div> */}
-                                            Home
-                                        </a>
-                                        <a href='#' className='flex  items-center gap-2 hover:translate-x-2 duration-300'>
-                                            <div className='h-1.5 w-1.5 bg-white rounded-full'></div>
-                                            Early Access
-                                        </a>
-                                        <a href='#' className='flex  items-center gap-2 hover:translate-x-2 duration-300'>
-                                            <div className='h-1.5 w-1.5 bg-white rounded-full'></div>
-                                            Gallery
-                                        </a>
-                                    </Disclosure.Panel>
-                                </Transition>
-                            </>
-                        )}
-                    </Disclosure>
-                </div>
-            </nav>
-            
-        </header>
-    )
+	let [isOpen, setIsOpen] = useState(false);
+
+	function handleSubmit(value) {
+		console.log(`User entered: ${value}`);
+		setIsOpen(false);
+	}
+
+	return (
+		// Making 2 navbar
+		<header className="fixed w-full z-50">
+			<nav className="hidden md:block bg-black text-white fixed h-screen p-3">
+				<div className="flex flex-col h-full gap-36 overflow-auto">
+					<div className="flex items-center justify-center gap-2">
+						<div>
+							<img className="w-16 h-16" src={NavBrand} alt="" />
+						</div>
+						<div className="font-bold text-xl mr-2">
+							DALL-E - 360
+						</div>
+					</div>
+					<div className="h-fulls flex items-center mb-20">
+						<div className="flex flex-col gap-3">
+							<a
+								href="#"
+								className="flex  items-center gap-2 hover:translate-x-2 duration-300"
+							>
+								<div className="h-1.5 w-1.5 bg-rose-600 rounded-full"></div>
+								Home
+							</a>
+							<a
+								href="#"
+								className="flex  items-center gap-2 hover:translate-x-2 duration-300"
+							>
+								<div className="h-1.5 w-1.5 bg-white rounded-full"></div>
+								Early Access
+							</a>
+							<a
+								href="#"
+								className="flex  items-center gap-2 hover:translate-x-2 duration-300"
+							>
+								<div className="h-1.5 w-1.5 bg-white rounded-full"></div>
+								Gallery
+							</a>
+							<button
+								onClick={() => setIsOpen(true)}
+								className="flex  items-center gap-2 hover:translate-x-2 duration-300"
+							>
+								<div className={`h-1.5 w-1.5 ${isOpen?"bg-rose-600":"bg-white"} rounded-full`}></div>
+								Prompt
+								<MyModal
+									isOpen={isOpen}
+									setIsOpen={setIsOpen}
+									onSubmit={handleSubmit}
+								/>
+							</button>
+						</div>
+					</div>
+					<a
+						href="#"
+						className="flex items-center gap-1 px-3 p-3 text-gray-200 fixed bottom-0"
+					>
+						<img className="w-10" src={discord} alt="" />
+						<div className="bg-gray-300 w-1 h-1 rounded-full"></div>
+						<div>Discord</div>
+					</a>
+				</div>
+			</nav>
+			<MobileNav
+				setIsOpen={setIsOpen}
+				isOpen={isOpen}
+				handleSubmit={handleSubmit}
+			/>
+		</header>
+	);
 }
-{/* <iframe className='block m-[20px] mx-auto border-0 max-w-[880px] rounded-[8px] ' width="90%" height="500px" allowFullScreen={true} allow="accelerometer; magnetometer; gyroscope" src="https://panoraven.com/en/embed/PYKxs5WSfX"></iframe> */}
-// style="display:block; margin:20px auto; border:0 none; max-width:880px;border-radius:8px; box-shadow: 0 1px 1px rgba(0,0,0,0.11),0 2px 2px rgba(0,0,0,0.11),0 4px 4px rgba(0,0,0,0.11),0 6px 8px rgba(0,0,0,0.11),0 8px 16px rgba(0,0,0,0.11);"
-// <div>
-//     <div className='flex font-bold text-lg items-center gap-2'>
-//         <div>
-//             <img className='w-12 h-12' src={NavBrand} alt="" />
-//         </div>
-//         <div>
-//             Dall-E - 360
-//         </div>
-//     </div>
-// </div>
+
+function MobileNav({ setIsOpen, isOpen, handleSubmit }) {
+	return (
+		<nav className="text-white bg-black w-full p-3 md:hidden">
+			<div>
+				<Disclosure>
+					{({ open }) => (
+						<>
+							<div className="flex justify-between">
+								<div className="flex items-center gap-2 font-bold text-lg">
+									<div>
+										<img
+											className="w-12 h-12"
+											src={NavBrand}
+											alt=""
+										/>
+									</div>
+									<div>Dall-E - 360</div>
+								</div>
+								<Disclosure.Button>
+									<div
+										className={`${
+											open ? "rotate-180" : "rotate-0"
+										}
+                                            transition-all duration-500 rounded-full border-2 p-0.5 flex items-center justify-center border-t-red-400`}
+									>
+										<ChevronDown />
+									</div>
+								</Disclosure.Button>
+							</div>
+							<Transition
+								show={open}
+								enter="transition duration-300 ease-out"
+								enterFrom="transform translate-y-full opacity-0"
+								enterTo="transform translate-y-0 opacity-100"
+								leave="transition duration-300 ease-out"
+								leaveFrom="transform translate-y-full opacity-100"
+								leaveTo="transform scale-50 opacity-0"
+							>
+								<Disclosure.Panel className="mt-2 p-2 font-medium space-y-2">
+									<a
+										href="#"
+										className="flex  items-center gap-2 hover:translate-x-2 duration-300"
+									>
+										<div className="h-1.5 w-1.5 bg-rose-600 rounded-full"></div>
+										Home
+									</a>
+									<a
+										href="#"
+										className="flex  items-center gap-2 hover:translate-x-2 duration-300"
+									>
+										<div className="h-1.5 w-1.5 bg-white rounded-full"></div>
+										Early Access
+									</a>
+									<a
+										href="#"
+										className="flex  items-center gap-2 hover:translate-x-2 duration-300"
+									>
+										<div className="h-1.5 w-1.5 bg-white rounded-full"></div>
+										Gallery
+									</a>
+									<button
+										onClick={() => setIsOpen(true)}
+										className="flex  items-center gap-2 hover:translate-x-2 duration-300"
+									>
+										<div className={`h-1.5 w-1.5 ${isOpen?"bg-rose-600":"bg-white"} rounded-full`}></div>
+										Prompt
+										<MyModal
+											isOpen={isOpen}
+											setIsOpen={setIsOpen}
+											onSubmit={handleSubmit}
+										/>
+									</button>
+								</Disclosure.Panel>
+							</Transition>
+						</>
+					)}
+				</Disclosure>
+			</div>
+		</nav>
+	);
+}
---
+++ src/prompt.jsx
@@ -0,0 +1,111 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import { classNames } from "./classNames";
+
+export default function MyModal({ isOpen, setIsOpen, onSubmit }) {
+	const [value, setValue] = useState("");
+
+	function closeModal() {
+		setIsOpen(false);
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		onSubmit(value);
+	}
+
+	return (
+		<>
+			<Transition appear show={isOpen} as={Fragment}>
+				<Dialog as="div" className="relative z-10" onClose={closeModal}>
+					<Transition.Child
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+					>
+						<div className="fixed inset-0 bg-black bg-opacity-25" />
+					</Transition.Child>
+
+					<div className="fixed inset-0 overflow-y-auto">
+						<div className="flex min-h-full items-center justify-center p-4 text-center">
+							<Transition.Child
+								as={Fragment}
+								enter="ease-out duration-300"
+								enterFrom="opacity-0 scale-95"
+								enterTo="opacity-100 scale-100"
+								leave="ease-in duration-200"
+								leaveFrom="opacity-100 scale-100"
+								leaveTo="opacity-0 scale-95"
+							>
+								<Dialog.Panel
+									className={classNames(
+										"w-full max-w-md rounded-2xl bg-white p-6",
+										"transform overflow-hidden align-middle shadow-xl transition-all"
+									)}
+								>
+									<Dialog.Title
+										as="h3"
+										className="text-lg font-medium leading-6 text-gray-900 text-center"
+									>
+										~ Enter Prompt ~
+									</Dialog.Title>
+									<form className="">
+										<div className="mt-4">
+											<input
+												className={classNames(
+													"border rounded-md",
+													"p-1.5 px-2 w-full",
+													"transition-all duration-300",
+													"focus:outline-none focus:input_shadow"
+												)}
+												type="text"
+												value={value}
+												onChange={(event) =>
+													setValue(event.target.value)
+												}
+											/>
+										</div>
+
+										<div className="mt-4 flex flex-col-reverse gap-2 md:flex-row">
+											<button
+												type="button"
+												className={classNames(
+													"bg-red-100 px-4 py-2 text-sm font-medium text-red-900 w-full",
+													"inline-flex justify-center",
+													"rounded-md border border-transparent",
+													"hover:bg-red-200 focus:outline-none",
+													"focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+												)}
+												onClick={closeModal}
+											>
+												Cancel
+											</button>
+											<button
+												type="submit"
+												// className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 w-full hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+												className={classNames(
+													"bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 w-full",
+													"inline-flex justify-center",
+													"rounded-md border border-transparent",
+													"hover:bg-blue-200 focus:outline-none",
+													"focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+												)}
+												onClick={handleSubmit}
+											>
+												Send!
+											</button>
+										</div>
+									</form>
+								</Dialog.Panel>
+							</Transition.Child>
+						</div>
+					</div>
+				</Dialog>
+			</Transition>
+		</>
+	);
+}
