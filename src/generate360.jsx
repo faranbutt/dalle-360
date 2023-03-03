0e806855e15b84aca126b6928f80a3a1f4596c79
faranbutt <faranbutt789@gmail.com>
2023-03-03 23:23:41 +0500

    Updated

--- src/generate360.jsx
+++ src/generate360.jsx
@@ -55,6 +55,7 @@ function ViewTypeDropdown() {
         <div className="flex items-center">
           <label htmlFor="environment-type" className="mr-4">Environment Type:</label>
           <select id="environment-type" value={environmentType} onChange={handleEnvironmentTypeChange} className="border border-gray-300 rounded-md p-2">
+          <option value="">Select Env Type</option>
             <option value="luxury-house">Luxury House</option>
             <option value="office-room">Office Room</option>
           </select>
